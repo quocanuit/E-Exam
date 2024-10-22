9b272d4982306ef86c89e84cb41c3355b50c692a
huudat <huudat81415@gmail.com>
2024-10-22 22:37:55 +0700

    Xây dựng userFragment

---
+++ app/src/main/java/com/example/e_exam/user/UserInfor.java
@@ -0,0 +1,57 @@
+package com.example.e_exam.user;
+
+public class UserInfor {
+    private String email;
+    private String birthday;
+    private String major;
+    private String hometown;
+    private String class_activity;
+
+    public UserInfor(String email, String birthday, String major, String hometown, String class_activity) {
+        this.email = email;
+        this.birthday = birthday;
+        this.major = major;
+        this.hometown = hometown;
+        this.class_activity = class_activity;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSpecialized() {
+        return major;
+    }
+
+    public void setSpecialized(String major) {
+        this.major = major;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getClass_activity() {
+        return class_activity;
+    }
+
+    public void setClass_activity(String class_activity) {
+        this.class_activity = class_activity;
+    }
+}
